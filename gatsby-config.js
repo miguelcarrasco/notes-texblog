557dfd05560c92844df439550c25e6ae2ce6739a
deon <tlacaelel.software@gmail.com>
2021-02-05 17:12:55 -0600

    Fixing grammar error on intro and SEO title

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
 	siteMetadata: {
 		// Update these and enjoy :)
 		title: `Ideas Interesantes`,
-		description: `Un blog con una compilacón de notas en matemáticas, ciencias de la computación, programación y otros temas interesantes, usado principalmente para recordar y compartir ideas.`,
+		description: `Un blog con una compilación de notas en matemáticas, ciencias de la computación, programación y otros temas interesantes, usado principalmente para recordar y compartir ideas.`,
 		author: `Miguel Angel Carrasco`,
 	},
 	plugins: [
